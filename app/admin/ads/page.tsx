7f977fddc439f5368d8caa5df6e56dd9efb0d2a6
itsaelok <samsulek692002@gmail.com>
2026-09-09 21:19:14 +0530

    Fix duplicate admin routes

--- app/admin/ads/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="p-8">
-      Ads Management
-    </div>
-  );
-}
--- app/admin/users/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="p-8">
-      Coming Soon
-    </div>
-  );
-}
